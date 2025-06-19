0b3775e504b884eb7cd3102e9f90a7f1f076c2aa
GitWithAnkit <ankitranjan251@gmail.com>
2025-06-19 18:56:25 +0530

    Update codes.js

--- codes.js
+++ codes.js
@@ -31,3 +31,37 @@ const countryList = {
   USD: "US",
   ZAR: "ZA"
 };
+
+const currencyNames = {
+  AUD: "Australian Dollar",
+  BGN: "Bulgarian Lev",
+  BRL: "Brazilian Real",
+  CAD: "Canadian Dollar",
+  CHF: "Swiss Franc",
+  CNY: "Chinese Yuan",
+  CZK: "Czech Koruna",
+  DKK: "Danish Krone",
+  EUR: "Euro",
+  GBP: "British Pound Sterling",
+  HKD: "Hong Kong Dollar",
+  HUF: "Hungarian Forint",
+  IDR: "Indonesian Rupiah",
+  ILS: "Israeli New Shekel",
+  INR: "Indian Rupee",
+  ISK: "Icelandic Krona",
+  JPY: "Japanese Yen",
+  KRW: "South Korean Won",
+  MXN: "Mexican Peso",
+  MYR: "Malaysian Ringgit",
+  NOK: "Norwegian Krone",
+  NZD: "New Zealand Dollar",
+  PHP: "Philippine Peso",
+  PLN: "Polish Zloty",
+  RON: "Romanian Leu",
+  SEK: "Swedish Krona",
+  SGD: "Singapore Dollar",
+  THB: "Thai Baht",
+  TRY: "Turkish Lira",
+  USD: "United States Dollar",
+  ZAR: "South African Rand"
+};
